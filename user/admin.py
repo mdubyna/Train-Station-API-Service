428cb59bf080a3bcff78f3e2108cca7305ef5b26
Mykola <mykola22dubyna@gmail.com>
2024-03-27 18:03:42 +0200

    Register models on admin panel

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from user.models import User
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = ("username", "first_name", "last_name", "is_staff")
+    search_fields = ("username", "first_name", "last_name")
