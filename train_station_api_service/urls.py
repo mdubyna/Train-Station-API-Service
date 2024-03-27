7aeee44fc00cb8d815c42af00b23d6bb701bfc09
Mykola <mykola22dubyna@gmail.com>
2024-03-27 14:27:35 +0200

    Add path to train_station and user apps urls files

--- train_station_api_service/urls.py
+++ train_station_api_service/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/train_station/", include("train_station.urls")),
+    path("api/user/", include("user.urls")),
 ]
