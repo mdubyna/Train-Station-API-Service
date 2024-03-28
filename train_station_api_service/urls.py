6dc48b6058756ea50eefc28cac80f700a741ea2c
Mykola <mykola22dubyna@gmail.com>
2024-03-28 17:57:25 +0200

    Add urls for docs, debug and train_station app endpoints

--- train_station_api_service/urls.py
+++ train_station_api_service/urls.py
@@ -17,9 +17,14 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/train_station/", include("train_station.urls")),
     path("api/user/", include("user.urls")),
+    path("__debug__/", include("debug_toolbar.urls")),
+    path("api/doc/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/doc/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
 ]
